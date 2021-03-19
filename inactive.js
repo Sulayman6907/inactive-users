9a6496a9bcd8ed2c5fb3ea1fee5c5aaedecf4a7f
Sulayman <muhammadsulaymannkhan@gmail.com>
2021-03-19 18:50:19 +0500

    Implemented Stage Printing

--- inactive.js
+++ inactive.js
@@ -5,6 +5,8 @@ const user = {
     lastActivity: new Date().getTime()
 }
 
+let stage = 0
+
 const inactiveFirstStage = () => {
     console.log("User has been inactive for the past 1-2 minutes");
 }
@@ -17,8 +19,41 @@ const inactiveThirdStage = () => {
     console.log("User has been inactive for the past 5-6 minutes");
 }
 
+function stagePrint(stage){
+    switch (stage) {
+        case 0:
+            inactiveFirstStage()
+            break
+        case 1:
+            inactiveSecondStage()
+            break
+        case 2:
+            inactiveThirdStage()
+            break
+    }
+}
+
 
 // this is the main method that acts like a worker and runs every 1 minute
 setInterval(function(){
     // please write your code here
-}, 1000);
+    console.log(stage)
+    const time = (new Date().getTime() - user.lastActivity)/(1000*60)
+    stagePrint(stage)
+
+    if (time < 2) {
+        stage = (stage +1 ) % 3
+    }
+    else if (time < 3 && time > 2) {
+        stage = (stage +1 ) % 3
+    }
+    else if (time < 5 && time > 4) {
+        stage = (stage +1 ) % 3
+    }
+    else {
+        stage = 0
+    }
+
+
+
+}, (1000 * 60));
